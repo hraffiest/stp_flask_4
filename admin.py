a8f83234461069877598a8e014a7ad2630518f80
Hrafnir <g@tgg.su>
2020-04-04 17:41:44 +0300

    admin dashboard in process

--- admin.py
+++ admin.py
@@ -8,7 +8,29 @@ class DashboardView(AdminIndexView):
     @expose('/')
     def index(self):
         applicants = db.session.query(Applicant).all()
-        return self.render('admin/admin_dashboard.html', apps=applicants)
+        appl_new = []
+        appl_r = []
+        for appl in applicants:
+            if appl.status == 'распределена в группу':
+                appl_r.append(appl)
+            if appl.status == 'новая':
+                appl_new.append(appl)
+        groups = db.session.query(Group).order_by(Group.start_date).all()
+        grops_new = []
+        for app in appl_new:
+            for g in groups:
+                if g.g_id == app.group_id and len(grops_new) < 4:
+                    grops_new.append(g)
+                elif len(grops_new) >= 3:
+                    break
+                if len(grops_new) >= 3:
+                    break
+        return self.render('admin/admin_dashboard.html',
+                           apps=applicants,
+                           r_apps=appl_r,
+                           n_apps=appl_new,
+                           groups=groups,
+                           grops_new=grops_new)
 
 
 class NewModel(ModelView):
