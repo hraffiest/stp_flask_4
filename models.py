3f6992c04edbc4fe591ee19c928206abb2725b42
Hrafnir <g@tgg.su>
2020-04-04 12:18:17 +0300

    user model is ready

--- models.py
+++ models.py
@@ -3,3 +3,21 @@ from werkzeug.security import generate_password_hash, check_password_hash
 
 db = SQLAlchemy()
 
+
+class User(db.Model):
+    u_id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    mail = db.Column(db.String, nulllable=False)
+    _password = db.Column(db.String, nullable=False)
+
+    @property
+    def password(self):
+        raise AttributeError('Не лезь к паролю')
+
+    @password.setter
+    def password(self, password):
+        self._password = generate_password_hash(password)
+
+    def password_valid(self, password):
+        return check_password_hash(self._password, password)
+
