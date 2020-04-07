5a29acd2a31bdf864336b6812199ad2a88608a1a
Hrafnir <g@tgg.su>
2020-04-07 11:29:01 +0300

    login and signup forms added

---
+++ forms.py
@@ -0,0 +1,25 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, HiddenField
+from wtforms.validators import DataRequired, Length, EqualTo, ValidationError, Email, InputRequired
+
+
+class SignUpForm(FlaskForm):
+    username = StringField('Ваше имя(email)',
+                           [Length(min=4, max=25, message='Ваше имя должно быть в пределах от 4-х до 25-ти символов'),
+                            InputRequired(message='Поле Ваше имя обязательно для заполнения'),
+                            Email(message='Email введен неверно')])
+    password = PasswordField('Пароль',
+                             [Length(min=8, max=25, message='Ваш пароль должно быть в пределах от 8 до 25-ти символов'),
+                              InputRequired(message='Поле Пароль обязательно для заполнения'),
+                              EqualTo('password_equal', message='Пароли не совпадают')])
+    password_equal = PasswordField('Повторите пароль')
+
+
+class LoginForm(FlaskForm):
+    username = StringField('Ваше имя(email)',
+                           [Length(min=4, max=25, message='Ваше имя должно быть в пределах от 4-х до 25-ти символов'),
+                            InputRequired(message='Поле Ваше имя обязательно для заполнения'),
+                            Email(message='Email введен неверно')])
+    password = PasswordField('Пароль',
+                             [Length(min=8, max=25, message='Пароль не верный'),
+                              InputRequired(message='Поле Пароль обязательно для заполнения')])
